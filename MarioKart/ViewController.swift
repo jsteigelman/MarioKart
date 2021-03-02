6cfe14df7ba35a772befc77849054fed7f2fed4b
Joey Steigelman <jhs430@nyu.edu>
2021-03-02 15:03:26 +0100

    User can move karts around the screen using a pan gesture.

--- MarioKart/ViewController.swift
+++ MarioKart/ViewController.swift
@@ -14,6 +14,22 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func didPanKartView(_ sender: UIPanGestureRecognizer) {
+        
+        //access the location property of the pan gesture recognizer
+        let location = sender.location(in: view)
+        
+        //print the current location returned from the gesture recognizer
+        print("Location: x: \(location.x), y: \(location.y)")
+        
+        
+        //access the view of the kart that was panned
+        let kartView = sender.view!
+        
+        //set the kart view's position to the current position of the gesture recognizer
+        kartView.center = location
+        
+    }
+    
 }
 
