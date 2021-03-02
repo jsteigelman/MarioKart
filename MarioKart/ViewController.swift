82b51469e7beac6ecbde01e698c7d5d93a09364e
Joey Steigelman <jhs430@nyu.edu>
2021-03-02 16:44:37 +0100

    User can long press the background to reset the karts

--- MarioKart/ViewController.swift
+++ MarioKart/ViewController.swift
@@ -9,12 +9,28 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    var kartOriginalCenter: CGPoint!
-
-
+    @IBOutlet weak var marioKartView: UIImageView!
+    @IBOutlet weak var bowserKartView: UIImageView!
+    @IBOutlet weak var warioKartView: UIImageView!
+    @IBOutlet weak var luigiKartView: UIImageView!
+    @IBOutlet weak var toadKartView: UIImageView!
+    
+    var marioStartingPoint = CGPoint()
+    var bowserStartingPoint = CGPoint()
+    var warioStartingPoint = CGPoint()
+    var luigiStartingPoint = CGPoint()
+    var toadStartingPoint = CGPoint()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        //store each kart's starting point when the app loads
+        marioStartingPoint = marioKartView.center
+        bowserStartingPoint = bowserKartView.center
+        warioStartingPoint = warioKartView.center
+        luigiStartingPoint = luigiKartView.center
+        toadStartingPoint = toadKartView.center
+
     }
 
     @IBAction func didPanKartView(_ sender: UIPanGestureRecognizer) {
@@ -46,9 +62,7 @@ class ViewController: UIViewController {
         
         //adjust the scale of the kart view using the scale from the pinch gesture recognizer
         kartView.transform = CGAffineTransform(scaleX: scale, y: scale)
-        
     }
-    
     
     
     @IBAction func didRotateKartView(_ sender: UIRotationGestureRecognizer) {
@@ -64,8 +78,8 @@ class ViewController: UIViewController {
         
         //adjust the rotation of the kart view using the rotation from the pinch gesture recognizer
         kartView.transform = CGAffineTransform(rotationAngle: rotation)
-    
     }
+    
     
     @IBAction func didTapKartView(_ sender: UITapGestureRecognizer) {
         
@@ -76,10 +90,38 @@ class ViewController: UIViewController {
         let kartView = sender.view!
         
         //animation of the kart
-        UIView.animate(withDuration: 0.5) {
+        UIView.animate(withDuration: 0.7) {
+            
+            //move kart
             kartView.center.x += 350
+            
+            //reset karts to starting location
+            self.marioKartView.center = self.marioStartingPoint
+            self.bowserKartView.center = self.bowserStartingPoint
+            self.warioKartView.center = self.warioStartingPoint
+            self.luigiKartView.center = self.luigiStartingPoint
+            self.toadKartView.center = self.toadStartingPoint
+            
+            //reset karts to their original states (restoring scale and rotation)
+            self.marioKartView.transform = CGAffineTransform.identity
+            self.bowserKartView.transform = CGAffineTransform.identity
+            self.warioKartView.transform = CGAffineTransform.identity
+            self.luigiKartView.transform = CGAffineTransform.identity
+            self.toadKartView.transform = CGAffineTransform.identity
         }
-    
-    
 }
+    
+    
+    @IBAction func didLongPressBackground(_ sender: UILongPressGestureRecognizer) {
+        
+        //reset the kart positions
+        marioKartView.center = marioStartingPoint
+        bowserKartView.center = bowserStartingPoint
+        warioKartView.center = warioStartingPoint
+        luigiKartView.center = luigiStartingPoint
+        toadKartView.center = toadStartingPoint
+    }
+    
+    
+    
 }
